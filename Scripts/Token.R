9c81b9c7d1f6ce9b5478a925c194d539b8315f99
chiaramoruzzi <chiaramoruzzi@gmail.com>
2022-12-13 14:16:10 +0100

    deletes token script 2

--- Scripts/Token.R
+++
@@ -1,8 +0,0 @@
-#Token
-
-token <- create_token(
-  app <- "26260108",
-  consumer_key <- 'zEoYJO30Mj6nbQVvobqTeN08n',
-  consumer_secret <- 'jNuX7ogU4KMvJtM35f6QdRfn033vQvnsMvzwfLNsOf5OZeo0fW',
-  access_token <- '4764428692-DZ4wXeY08fLSTITp9XJ33Nf6aQ1kpSRAr3EfX1P',
-  access_secret <- 'xKMuMfqwHitoSL0Pu8fmQlHdN5riTFBcDN2kxfWnH4gIo')
