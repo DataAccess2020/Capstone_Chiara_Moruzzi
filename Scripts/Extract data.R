d8c491c5bfa75625fc21acd396ef641a279ffb4a
chiaramoruzzi <chiaramoruzzi@gmail.com>
2022-12-15 14:37:36 +0100

    organizes scripts better (i removed unsuccessful attempts from the final scripts and moved them to "Notes")

--- Scripts/Extract data.R
+++ Scripts/Extract data.R
@@ -1,110 +1,34 @@
-# TO DO LIST
+#list download
 
-library(tidyverse) 
-library(rvest)
-library(stringr)
-library(httr)
-library(RCurl)
-
-
-url <- "https://www.camera.it/leg19/29?tipoAttivita=&tipoVisAtt=&tipoPersona=&shadow_deputato=307394&idLegislatura=19"
-
-parsed_page <- read_html(url)
-
-
-# First try: twitter handle and personal data for one MP
-dat_aiello <- tibble(
-  
-  twitter_handle = parsed_page %>%
-    
-    html_element(css = ".twitter a") %>%
-    
-    html_attr(name = "href"),
-  
-  dati_biografici = parsed_page %>%
-    
-    html_element(css = ".datibiografici") %>%
-    
-    html_text() %>%
-    
-    stringr::str_replace_all(pattern = "\\s", replacement = " ")
-  
+italian_MP_under40 = lists_members(
+  list_id = "1601560531244093440",
+  slug = NULL,
+  owner_user = NULL,
+  n = 48,
+  cursor = "-1",
+  token = NULL,
+  retryonratelimit = TRUE,
+  verbose = TRUE,
+  parse = TRUE,
 )
 
-# for loop to extract twitter handles and personal data for all the politicians
 
-url_gen <- "https://www.camera.it/leg19/28"
-email <- "chiaramoruzzi@gmail.com"
-agent <- R.Version()$version.string
-user_id <- c(From = email, `User-Agent` = agent)
+#string vector with all under
+MP_under40_names = c(italian_MP_under40$name)
+print(MP_under40_names)
 
-web_page <- RCurl::getURL(url=url, httpheader= (user_id))
-all_link = XML::getHTMLLinks(web_page, externalOnly = T)
+MP_under40_t_handle = c(italian_MP_under40$screen_name)
+print(MP_under40_t_handle)
 
-all_link
+MP_under40_names[24] # ok, now we can iterate with a general function
+MP_under40_t_handle[24]
 
-# EXTRACT PAGES WITH MP DIVIDED BY LETTER
+#I create a dataframe containing all the names with the corresponding twitter handles of all the under 40 years old MPs who have twitter
+DF_MP_under40 <- tibble (MP_under40_names,MP_under40_t_handle)
 
-#pages_AZ = str_extract(all_link, pattern = "^https://documenti.camera.it.*&lettera=")
-#print (pages_AZ)
-#NON FUNZIONA
-
-# CREO LA MIA FUNZIONE DOWNLOAD POLITELY
-
-download_politely <- function(from_url, to_html, my_email, my_agent = R.Version()$version.string) {
-  
-  require(httr)
-  
-  stopifnot(is.character(from_url))
-  stopifnot(is.character(to_html))
-  stopifnot(is.character(my_email))    
-  
-  MP_req <-httr::GET(url = url[i],   
-                        add_headers(
-                          From = email, 
-                          `User-Agent` = R.Version()$version.string
-                        )
-  )
-  
-  if (httr::http_status(MP_req)$message == "Success: (200) OK") {
-    bin <- content(MP_req, as = "raw")
-    writeBin(object = bin, con = to_html)
-  } else {
-    cat("Houston, we have a problem!")
-  }
-}
-
-#FOR LOOP
-
-dir.create("MP personal pages")
-
-page <- "https://www.camera.it/leg19/28"
-prova_AZ = all_link[[i]] %>%                # the main page, categories of the website and images)
-  str_subset(page)
-prova_AZ %>% 
-  str_extract(all_link[[i]])
+#TWEETS EXTRACTION
 
 
-url_A <- "https://www.camera.it/leg19/29?tipoAttivita=&tipoVisAtt=&tipoPersona=&shadow_deputato="
-
-#links_AZ <- str_c(url_A <- "https://www.camera.it/leg19/28?lettera=")
-#links_AZ
-
-
-for (i in seq_along(url_A[[i]])) {
-  download_politely(from_url = url_A[[i]], 
-                    to_html = here::here("MP personal pages", str_c("deputato=",i,".html")), 
-                    my_email = email)
-  
-  Sys.sleep(0.5)  # We use a sys.sleep of 0.5 seconds to avoid being banned from the server
-}
-
-
-#--------------------------------------------------------------
-#cambio metodo
-
-
-#scarico liste twitter già divise per età
 
 
 
--- Scripts/Twitter login.R
+++ Scripts/Twitter login.R
@@ -14,33 +14,8 @@ aut_token <- rtweet_app(token)
 auth_setup_default()
 
 
-#list download
-
-italian_MP_under40 = lists_members(
-  list_id = "1601560531244093440",
-  slug = NULL,
-  owner_user = NULL,
-  n = 48,
-  cursor = "-1",
-  token = NULL,
-  retryonratelimit = TRUE,
-  verbose = TRUE,
-  parse = TRUE,
-)
 
 
-#string vector with all under
-MP_under40_names = c(italian_MP_under40$name)
-print(MP_under40_names)
-
-MP_under40_t_handle = c(italian_MP_under40$screen_name)
-print(MP_under40_t_handle)
-
-MP_under40_names[24] # ok, now we can iterate with a general function
-MP_under40_t_handle[24]
-
-#I create a dataframe containing all the names with the corresponding twitter handles of all the under 40 years old MPs who have twitter
-DF_MP_under40 <- tibble (MP_under40_names,MP_under40_t_handle)
 
 
 
